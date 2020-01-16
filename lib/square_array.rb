43c843c2662d8ab1a1fa6287db6b107d667a5b72
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 04:45:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  new_array = []
-  array.each do |x|
-    new_array.push x*x
- end
- new_array
+  counter = 0
+  while counter < array.length() 
+    array[counter] = array[counter] ** 2 
+    counter++
+  end
+  array
 end
